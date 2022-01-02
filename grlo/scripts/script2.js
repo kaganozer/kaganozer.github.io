db249353412b5b72c4c20ffe65fa487febb4e343
Kağan Özer <kgnozr2006@gmail.com>
2022-01-02 18:02:56 +0300

    Update

---
+++ grlo/scripts/script2.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides2(slideIndex);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("ürün");
+  var dots = document.getElementsByClassName("ürünR");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active2", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active2";
+}
