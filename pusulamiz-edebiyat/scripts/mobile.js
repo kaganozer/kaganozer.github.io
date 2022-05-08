747d5f1f8f21f66cc430fed284bd2e14cb70a2e2
Kağan Özer <kgnozr2006@gmail.com>
2022-05-08 19:54:36 +0300

    fixing the problem about displaying details elements

--- pusulamiz-edebiyat/scripts/mobile.js
+++ pusulamiz-edebiyat/scripts/mobile.js
@@ -12,83 +12,31 @@ document.querySelector("#mobileTheme").addEventListener("change", function(){
     }
 })
 
+
 class Accordion {
-    constructor(el) {
-        this.el = el;
-        this.summary = el.querySelector('summary');
-        this.content = el.querySelector('.content');
-        
-        this.animation = null;
-        this.isClosing = false;
-        this.isExpanding = false;
-        this.summary.addEventListener('click', (e) => this.onClick(e));
-    }
-  
-    onClick(e) {
-        e.preventDefault();
-        this.el.style.overflow = 'hidden';
-        if (this.isClosing || !this.el.open) {
-          this.open();
-        } else if (this.isExpanding || this.el.open) {
-          this.shrink();
-        }
-    }
-  
-    shrink() {
-        this.isClosing = true;
-        
-        const startHeight = `${this.el.offsetHeight}px`;
-        const endHeight = `${this.summary.offsetHeight}px`;
-        
-        if (this.animation) {
-          this.animation.cancel();
-        }
-        
-        this.animation = this.el.animate({
-          height: [startHeight, endHeight]
-        }, {
-          duration: 400,
-          easing: 'ease-out'
-        });
-        
-        this.animation.onfinish = () => this.onAnimationFinish(false);
-        this.animation.oncancel = () => this.isClosing = false;
-    }
-  
-    open() {
-        this.el.style.height = `${this.el.offsetHeight}px`;
-        this.el.open = true;
-        window.requestAnimationFrame(() => this.expand());
-    }
-  
-    expand() {
-        this.isExpanding = true;
-        const startHeight = `${this.el.offsetHeight}px`;
-        const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
-        
-        if (this.animation) {
-          this.animation.cancel();
-        }
-      
-        this.animation = this.el.animate({
-          height: [startHeight, endHeight]
-        }, {
-          duration: 400,
-          easing: 'ease-out'
-        });
-        this.animation.onfinish = () => this.onAnimationFinish(true);
-        this.animation.oncancel = () => this.isExpanding = false;
-    }
-  
-    onAnimationFinish(open) {
-        this.el.open = open;
-        this.animation = null;
-        this.isClosing = false;
-        this.isExpanding = false;
-        this.el.style.height = this.el.style.overflow = '';
-    }
+  constructor(el) {
+      this.el = el;
+      this.summary = el.querySelector(".summary");
+      this.content = el.querySelector(".content");
+
+      this.isClosed = true;
+      this.el.style.overflow = "hidden";
+      this.el.style.height = `${this.summary.offsetHeight}px`;
+      this.summary.addEventListener("click", e => this.onClick(e));
+  }
+
+  onClick(e) {
+      e.preventDefault();
+      if (this.isClosed) {
+          this.el.style.height = `${this.summary.offsetHeight + this.content.offsetHeight}px`
+          this.isClosed = false;
+      } else {
+          this.el.style.height = `${this.summary.offsetHeight}px`
+          this.isClosed = true;
+      }
+  }
 }
-  
-document.querySelectorAll('.ilce details').forEach((el) => {
-    new Accordion(el);
-});
+
+document.querySelectorAll(".accordion").forEach((el) => {
+  new Accordion(el);
+})
