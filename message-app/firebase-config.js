4c798cfeb3636cb56558f4feb9a5a2c3a156efa2
Kağan Özer <kgnozr2006@gmail.com>
2022-09-08 14:12:29 +0300

    Removed .gitignore

---
+++ message-app/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7Ce4k6VhbH2lSwNNhovomBFE7mcrRjVs",
+    authDomain: "message-application-github.firebaseapp.com",
+    projectId: "message-application-github",
+    storageBucket: "message-application-github.appspot.com",
+    messagingSenderId: "71096389390",
+    appId: "1:71096389390:web:8de7bf81abbe559e77515f",
+    databaseURL: "https://message-application-github-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+export { firebaseConfig };
